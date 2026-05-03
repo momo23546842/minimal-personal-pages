eb1f4626ac793d5e0336380a344fb873d82b6904
Momoyo Kataoka <m.kataoka53@gmail.com>
2026-05-03 18:05:37 +1000

    fix(theme): force light mode on initial load

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html:
-              `(function(){try{const t=localStorage.getItem('theme');if(t){document.documentElement.classList.remove('light','dark');document.documentElement.classList.add(t);document.documentElement.style.colorScheme = t === 'dark' ? 'dark' : 'light';}}catch(e){}})();`,
+              `(function(){try{document.documentElement.classList.remove('light','dark');document.documentElement.classList.add('light');document.documentElement.style.colorScheme = 'light';localStorage.setItem('theme','light')}catch(e){}})();`,
           }}
         />
         <ThemeProvider>
